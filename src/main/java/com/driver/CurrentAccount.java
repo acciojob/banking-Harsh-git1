365e82d09c71224c4cd33069967004faf6812283
Harsh Srivastava <srivastavaharsh8957@gmail.com>
2023-08-22 14:28:00 +0530

    account functionality

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,10 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name, balance, 5000);
 
+        if(balance < 5000) throw new Exception("Insufficient Balance");
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +17,37 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        char[] licenseChars = tradeLicenseId.toCharArray();
+        boolean isValid = true;
+
+        for (int i = 1; i < licenseChars.length; i++) {
+            if (licenseChars[i] == licenseChars[i - 1]) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid) {
+            return; // License ID is already valid
+        }
+
+        // Rearrange characters to create a valid license ID
+        for (int i = 1; i < licenseChars.length; i += 2) {
+            if (licenseChars[i] == licenseChars[i - 1]) {
+                int j = i + 1;
+                while (j < licenseChars.length && licenseChars[j] == licenseChars[i - 1]) {
+                    j++;
+                }
+                if (j == licenseChars.length) {
+                    throw new Exception("Valid License can not be generated");
+                }
+                char temp = licenseChars[j];
+                licenseChars[j] = licenseChars[i];
+                licenseChars[i] = temp;
+            }
+        }
+
+        tradeLicenseId =  new String(licenseChars);
     }
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -6,23 +6,40 @@ public class SavingsAccount extends BankAccount{
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
-
+        super(name, balance, 0);
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate = rate;
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
+        if(amount > maxWithdrawalLimit) throw new Exception("Maximum Withdraw Limit Exceed");
+        else if(amount > getBalance()) throw new Exception("Insufficient Balance");
+        else setBalance(getBalance() - amount);
 
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
+        double P = getBalance();
+        double R = rate;
+        double T = years;
 
+        return P + ((P*R*T)/100);
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
+        double principal = getBalance();
+        double compoundingFrequency = times;
+        // Convert rate to a fraction
+        double rateFraction = rate / 100.0;
 
+        // Calculate compound interest
+        double amount = principal * Math.pow(1 + rateFraction / compoundingFrequency, compoundingFrequency * years);
+
+        return amount;
     }
 
 }
